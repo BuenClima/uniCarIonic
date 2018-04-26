8906132ab420694f1ec25d3cfee080d7aa7cb14f
BuenClima <diegomartin9495@gmail.com>
2018-04-26 01:08:05 +0100

    Tests against server requests successfull

--- src/providers/restfull/restfull.ts
+++ src/providers/restfull/restfull.ts
@@ -10,26 +10,72 @@ export class RestfullProvider {
     console.log('Hello RestfullProvider Provider');
   }
 
+  private static buildHeaders(params : Array<string>){
+    return new HttpHeaders({'Content-Type' : params[0],
+                                    'access-token' : params[1],
+                                    'client' : params[2],
+                                    'uid' : params[3]});
+  }
+
   public signIn(){
-    let contentHeader = new HttpHeaders({"Content-Type": "application/json"});
+    var headers = RestfullProvider.buildHeaders(["application/json","","",""]);
     return this.http.post(this.baseUrl + '/auth/sign_in',
-      { "email" : "test@test.com",
-              "password" : "testpassword1"
+      {
+        "email": "test@test.com",
+        "password": "testpassword1"
+
       },
       {
-        headers : contentHeader,
+        headers : headers,
         observe : "response"
       }
       ).subscribe(
       (resp) => {
         console.log("resp-ok");
-        console.log(resp.headers);
+        console.log(resp.headers.keys().map(key => `${key}: ${resp.headers.get(key)}`));
+        console.log(resp.body)
+        this.getAllCars(resp.headers.keys().map(key => `${key}: ${resp.headers.get(key)}`));
       },
       (resp) => {
         console.log("resp-error");
         console.log(resp);
       }
     );
+  }
+
+  public getAllCars(params :Array<string> ){
+    var token = "";
+    var uid = "";
+    var client = "";
+    for (let param of params){
+      if (param.startsWith("access")){
+        token = RestfullProvider.formatParameters(param);
+      }
+      if (param.startsWith("client")){
+        client = RestfullProvider.formatParameters(param);
+      }
+      if (param.startsWith("uid")) {
+        uid = RestfullProvider.formatParameters(param);
+      }
+    }
+    var headers = RestfullProvider.buildHeaders(["application/json",token,client,uid]);
+
+    return this.http.get(this.baseUrl + '/v1/cars', {
+      headers : headers,
+      observe : "response"
+    }).subscribe(
+      (response) => {
+          console.log(response.body);
+      },(response) => {
+        console.log("resp-error");
+        console.log(response);
+      }
+    );
+  }
+
+
+  private static formatParameters(param : string){
+    return param.split(":")[1].trim();
   }
 
 }
