bc8717e420fa0be742e436b7bf92d7671564401b
Kevin <krealina24@hotmail.es>
2018-04-28 22:00:29 +0100

    Validation in forms done

--- src/pages/register-user/register-user.ts
+++ src/pages/register-user/register-user.ts
@@ -21,11 +21,17 @@ import { SearchTripPage } from "../search-trip/search-trip";
 export class RegisterUserPage {
 
   myForm: FormGroup;
-  mensaje: string;
+  mensajeName: string;
+  mensajeLastname: string;
+  mensajeBirthdate: string;
+  mensajeEmail: string;
+  mensajePassword: string;
+  mensajeConfirmPassword: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public restful: UserRestfulServiceProvider,
               public menuService: MenuServiceProvider) {
     this.myForm = this.createMyForm();
+    console.log (this.myForm);
   }
 
   onSubmit(){
@@ -46,7 +52,26 @@ export class RegisterUserPage {
         this.menuService.addPages(pages);
         this.navCtrl.setRoot(SearchTripPage);
       },(response) => {
-        this.mensaje ="Introduzca correctamente los datos";
+         console.log (response);
+         if(this.myForm.controls.name.value == "") {
+           this.mensajeName = "El nombre no puede estar vacío";
+         }
+         if(this.myForm.controls.lastname.value == "") {
+           this.mensajeLastname = "Los apellidos no pueden estar vacíos";
+         }
+         if(this.myForm.controls.birthdate.value == "") {
+          this.mensajeBirthdate = "Seleccione una fecha";
+         }
+         if(this.myForm.controls.password.value.length < 6) {
+           this.mensajePassword = "La contraseña debe tener al menos 6 carácteres";
+         }
+         if(this.myForm.controls.password_confirmation.value != this.myForm.controls.password.value) {
+           this.mensajeConfirmPassword = "Las contraseñas no coinciden";
+         }
+         if(!this.myForm.controls.email.value.match(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+           this.mensajeEmail = "Introduzca un formato correcto de email";
+         }
+
       }
     );
   }
